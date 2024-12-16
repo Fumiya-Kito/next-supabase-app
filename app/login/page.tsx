6754c04c23300cfadf7ac3dbf5b4a613b2a8ce6a
Fumiya-Kito <cfbc.pearl@icloud.com>
2024-12-16 23:36:32 +0900

    feat: add redirect logic, ref: subdivide header and footer modules into components

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,23 @@
 import { currentUser } from "../_data/auth";
 import { Button } from "@/components/ui/button";
 import { signIn, signOut } from "../_actions/auth";
+import { redirect } from "next/navigation";
 
 export default async function () {
   const user = await currentUser();
+
+  if (user) {
+    redirect('/mypage')
+  }
   return (
     <div className="p-6">
       <h1>LOGIN</h1>
       {user && JSON.stringify(user)}
 
-      {user ? (
-        <form action={signOut}>
-          <Button>ログアウト</Button>
-        </form>
-      ) : (
-        <form action={signIn}>
-          <Button>ログイン</Button>
-        </form>
-      )}
+
+      <form action={signIn}>
+        <Button>ログイン</Button>
+      </form>
     </div>
   );
 }
---
+++ components/header.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { currentUser } from "@/app/_data/auth";
+import { signIn, signOut } from "@/app/_actions/auth";
+
+export default async function Header() {
+  const user = await currentUser();
+  return (
+    <header className="h-16 border-b gap-3 px-6 flex items-center">
+      <Button asChild variant={'ghost'} className="font-bold text-xl">
+        <Link href="/">LOGO</Link>
+      </Button>
+      <Button asChild variant={'ghost'} className="font-bold text-xl">
+        <Link href="/about">About</Link>
+      </Button>
+      <Button asChild variant={'ghost'} className="font-bold text-xl">
+        <Link href="/mypage">MyPage</Link>
+      </Button>
+
+      <span className="flex-1"></span>
+
+      {user ? (
+        <form action={signOut}>
+          <Button variant={"outline"}>ログアウト</Button>
+        </form>
+      ) : (
+        <form action={signIn}>
+          <Button>ログイン</Button>
+        </form>
+      )}
+    </header>
+
+  )
+}
